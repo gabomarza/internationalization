8c47bd511d830e7e042b9467ded30fb3893e0a38
Gabriel Martínez Zarama <gabomarza@hotmail.com>
2019-04-02 13:42:36 -0500

    Desarrollo completo del ejercicio

--- src/components/job.js
+++ src/components/job.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { FormattedDate } from 'react-intl';
+import { FormattedRelative } from 'react-intl';
+import { FormattedPlural } from 'react-intl';
+import { FormattedNumber } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 export default class Job extends React.Component {
 
-  	render() {
-  		return (
-  			<tr>
-  				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
-      			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
-  			</tr>
-  		);
-	}
+    getMillion(){
+      return <FormattedMessage id="Million"/>;
+    }
+    getMillions(){
+      return <FormattedMessage id="Millions"/>;
+    }
+    render() {
+        return (
+            <tr>
+          <th scope="row">{this.props.offer.id}</th>
+            <td>{this.props.offer.name}</td>
+            <td>{this.props.offer.company}</td>
+            <td>{this.props.offer.salary}
+                <FormattedPlural
+                value={this.props.offer.salary}
+                one={this.getMillion()}
+                other={this.getMillions()}
+                />
+            </td>
+            <td>{this.props.offer.city}</td>
+            <td>
+                <p> 
+                <FormattedDate
+                  value={new Date(this.props.offer.date)}
+                  year='numeric'
+                  month='long'
+                  day='numeric'
+                  weekday='long'
+                /> ({<FormattedRelative value={new Date(this.props.offer.date)}/>})</p>
+            </td>
+            <td>
+                <FormattedNumber value ={this.props.offer.visits}/>
+            </td>
+        </tr>
+        );
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import JobsList from "./components/jobsList";
+import esLocaleData from 'react-intl/locale-data/es';
+import {IntlProvider, addLocaleData} from 'react-intl';
+import menssagesEsp from './locales/es';
+import menssagesEn from './locales/en';
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData(esLocaleData);
+var userLang = navigator.language || navigator.userlanguage;
+
+
+function getZona(){
+	if(userLang=='es-ES'){
+		return menssagesEsp;
+	}else{
+		return menssagesEn;
+	}
+}
+
+ReactDOM.render(
+	<IntlProvider locale={userLang} messages={getZona()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
